cdd5398d3afd1c8645699a8344a59321fb526fbe
Mike Harmon <mdh266@gmail.com>
2023-11-21 20:52:46 -0500

    simplier method for capital gains

--- leetcode/capital-gainloss.sql
+++ leetcode/capital-gainloss.sql
@@ -11,3 +11,15 @@ FROM
 GROUP BY 1
 ) T
 ;
+
+-- or
+
+SELECT
+    stock_name,
+    SUM(CASE WHEN operation = 'Buy' THEN -price
+             ELSE price 
+        END) AS capital_gain_loss
+FROM
+    Stocks
+GROUP BY 1
+;
