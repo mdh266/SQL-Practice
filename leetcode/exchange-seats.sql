b0d939fb8c3dbf412b60ffb6da4bc1707013c258
Mike Harmon <mdh266@gmail.com>
2023-11-21 21:39:27 -0500

    exchange seats multiple solutions

--- leetcode/exchange-seats.sql
+++ leetcode/exchange-seats.sql
@@ -1,3 +1,6 @@
+
+-- Soluton 1, 251 ms
+
 SELECT 
     id,
     CASE WHEN id % 2 != 0 AND id < (SELECT MAX(id) FROM seat) THEN lead_student
@@ -13,4 +16,37 @@ FROM (
     FROM 
         seat
 ) base
+;
+
+-- Solution 2, 601 ms
+SELECT
+    id,
+    CASE WHEN MOD(id, 2) = 0 THEN prev_student 
+         WHEN next_id IS NOT NULL THEN next_student
+         ELSE student
+    END AS student
+FROM (
+    SELECT
+        id,
+        student,
+        LAG(student, 1) OVER(ORDER BY id ASC) prev_student,
+        LEAD(student, 1) OVER(ORDER BY id ASC) next_student,
+        LEAD(id, 1) OVER(ORDER BY id ASC) next_id
+    FROM
+        Seat
+) T
+;
+
+-- Solution 3, 590ms
+
+SELECT
+    CASE WHEN MOD(id, 2) = 0 THEN id - 1
+         WHEN id < (SELECT MAX(id) FROM Seat) THEN id + 1
+         ELSE id
+    END AS id,
+    student
+FROM
+     Seat
+ORDER BY 
+    id ASC
 ;
